2ded94191c2c0922c56850ba301e52e4c1a2d5cc
Chandra Holt <chandra_holt@hotmail.com>
2022-08-29 22:09:43 -0700

    Implementing hooks and state to store array of student names. TODO: Fix addName form

--- src/components/StudentList/index.js
+++ src/components/StudentList/index.js
@@ -1,6 +1,5 @@
 import * as React from "react";
-import { useState } from "react";
-import ReactDOM from "react-dom";
+import { useState, useEffect } from "react";
 import {
   Box,
   Grid,
@@ -13,53 +12,91 @@ import {
   Chip,
 } from "@mui/material";
 
+//Styling
 const fieldStyle = {
   background: "white",
 };
 
-// const [studentNames, setStudentNames] = useState({
-//   studentNameInput: "",
-// });
-
-let studentNames = [
-  "Mary Smith",
-  "Joaquim Tochtermann",
-  "Ahmed Mohammed",
-  "Jules Reynolds",
-  "Tuan Hong",
-  "Bob Ray",
-  "Liam Neeson",
-  "Jose Gallo",
-  "Nicole Herman",
-];
-
-// TODO: Create function to map over student names after seeds are ready
-const handleGenerate = () => {
-  console.log("The generate random button is working!");
-  // studentNames.concat();
-};
-
-const handleAddName = () => {
-  console.log("The add name button is working!");
-};
-
-console.log(studentNames);
-
+//Declare export here
 const StudentList = () => {
+  //Initialize student list as an empty array
+  const [studentNames, setStudentNames] = useState([
+    "Larry Smith",
+    "Dora Jones",
+    "Ahmed Mohamed",
+    "Tuan Hong",
+  ]);
+  //set local storage items as student names
+  useEffect(() => {
+    localStorage.setItem("studentNames", studentNames);
+  });
+  console.log("Here is the first test", studentNames);
+
+  //dummy list for student names for testing
+  // let studentNames = [
+  //   "Mary Smith",
+  //   "Joaquim Tochtermann",
+  //   "Ahmed Mohammed",
+  //   "Jules Reynolds",
+  //   "Tuan Hong",
+  //   "Bob Ray",
+  //   "Liam Neeson",
+  //   "Jose Gallo",
+  //   "Nicole Herman",
+  // ];
+
+  //initialize random student variable
+  let randomStudent;
+
+  // TODO: Create function to map over student names after seeds are ready
+  const handleGenerate = () => {
+    console.log("The generate random button is working!");
+    randomStudent =
+      studentNames[Math.floor(Math.random() * studentNames.length)];
+  };
+
+  const [formState, setFormState] = useState({
+    studentName: "",
+  });
+
+  //When user enters a name into the text field, declare this as target for form state
+  const handleFormChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  //Add entered name to array
+  const handleAddName = async (event) => {
+    console.log(formState);
+
+    console.log("The add name button is working!");
+
+    setStudentNames((studentNames) => studentNames.concat());
+  };
+
+  console.log(studentNames);
+
   return (
     <Box>
       <FormControl>
+        <p>Enter a student's name and click "ADD NAME TO LIST".</p>
         <TextField
           id="student-name-form"
           variant="outlined"
           label="Write a name"
           color="primary"
+          onChange={handleFormChange}
           sx={fieldStyle}
         ></TextField>
-        <p>Enter a student's name and click "ADD NAME TO LIST".</p>
+
         <Button
           id="add-name-button"
           variant="contained"
+          sx={{ mt: 4, mb: 2 }}
           onClick={() => handleAddName()}
         >
           Add name to list
@@ -72,8 +109,12 @@ const StudentList = () => {
         >
           Generate a random name
         </Button>
-        <Chip variant="outlined" sx={{ mt: 4, mb: 2 }} color="primary"></Chip>{" "}
-      
+        <Chip
+          variant="outlined"
+          label={randomStudent}
+          sx={(fieldStyle, { mt: 4, mb: 2 })}
+          // color="primary"
+        />
       </FormControl>
 
       <Grid container spacing={2}>
